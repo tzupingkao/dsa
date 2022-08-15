97a54ed51c4f7273a58619da9a719ed6f0f794af
tzuping <tzupingkao@gmail.com>
2022-08-15 19:53:59 +0800

    add merge_sort function

--- merge_sort.py
+++ merge_sort.py
@@ -1,6 +1,4 @@
 
-from operator import le
-from re import A
 
 
 def merge_sort(array):
@@ -12,9 +10,37 @@ def merge_sort(array):
         left_array = merge_sort(left_array)
         right_array = merge_sort(right_array)
 
-        tzu = 1
+        left_ind = 0
+        right_ind = 0
+        result_ind = 0
+        result_array = [None] * len(array)
+        while left_ind < len(left_array) and right_ind < len(right_array):
+            if left_array[left_ind] < right_array[right_ind]:
+                result_array[result_ind] = right_array[right_ind]
+                right_ind = right_ind + 1
+            else:
+                result_array[result_ind] = left_array[left_ind]
+                left_ind = left_ind + 1
+            
+            result_ind = result_ind + 1
+
+        while right_ind < len(right_array):
+            result_array[result_ind] = right_array[right_ind]
+            right_ind = right_ind + 1
+            result_ind = result_ind + 1
+
+        while left_ind < len(left_array):
+            result_array[result_ind] = left_array[left_ind]
+            left_ind = left_ind + 1
+            result_ind = result_ind + 1
+        
+        return result_array
+    else:
+        return array
+
+    
 
 if __name__ == '__main__':
     Numbers = [41, 33, 17, 80, 61, 5, 55]
-    merge_sort(Numbers)
-    print(Numbers)
+    result = merge_sort(Numbers)
+    print(result)
